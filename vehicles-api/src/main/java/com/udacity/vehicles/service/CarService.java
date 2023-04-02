c794389d0b9a0a00f64b419548c42b23fd4e9e93
cch <aehtla2012@gmail.com>
2023-04-02 21:59:49 +0800

    [IMP] update/delete car done

--- vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -26,7 +26,7 @@ public class CarService {
 
     public CarService(CarRepository repository, MapsClient mapsClient, PriceClient priceClient) {
         /**
-         * TODO: Add the Maps and Pricing Web Clients you create
+         * [DONE] TODO: Add the Maps and Pricing Web Clients you create
          *   in `VehiclesApiApplication` as arguments and set them here.
          */
         this.repository = repository;
@@ -49,7 +49,7 @@ public class CarService {
      */
     public Car findById(Long id) {
         /**
-         * TODO: Find the car by ID from the `repository` if it exists.
+         * [DONE] TODO: Find the car by ID from the `repository` if it exists.
          *   If it does not exist, throw a CarNotFoundException
          *   Remove the below code as part of your implementation.
          */
@@ -61,9 +61,9 @@ public class CarService {
         Car car = optionalCar.get();
 
         /**
-         * TODO: Use the Pricing Web client you create in `VehiclesApiApplication`
+         * [DONE] TODO: Use the Pricing Web client you create in `VehiclesApiApplication`
          *   to get the price based on the `id` input'
-         * TODO: Set the price of the car
+         * [DONE] TODO: Set the price of the car
          * Note: The car class file uses @transient, meaning you will need to call
          *   the pricing service each time to get the price.
          */
@@ -71,10 +71,10 @@ public class CarService {
         car.setPrice(price);
 
         /**
-         * TODO: Use the Maps Web client you create in `VehiclesApiApplication`
+         * [DONE] TODO: Use the Maps Web client you create in `VehiclesApiApplication`
          *   to get the address for the vehicle. You should access the location
          *   from the car object and feed it to the Maps service.
-         * TODO: Set the location of the vehicle, including the address information
+         * [DONE] TODO: Set the location of the vehicle, including the address information
          * Note: The Location class file also uses @transient for the address,
          * meaning the Maps service needs to be called each time for the address.
          */
@@ -107,15 +107,18 @@ public class CarService {
      */
     public void delete(Long id) {
         /**
-         * TODO: Find the car by ID from the `repository` if it exists.
+         * [DONE] TODO: Find the car by ID from the `repository` if it exists.
          *   If it does not exist, throw a CarNotFoundException
          */
-
+        Optional<Car> optionalCar = repository.findById(id);
+        if(optionalCar.isEmpty()){
+            throw new CarNotFoundException();
+        }
+        Car car = optionalCar.get();
 
         /**
-         * TODO: Delete the car from the repository.
+         * [DONE] TODO: Delete the car from the repository.
          */
-
-
+        repository.delete(car);
     }
 }
